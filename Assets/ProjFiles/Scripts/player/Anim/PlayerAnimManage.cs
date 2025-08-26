808458be5aec16359b3f679036950dd96e6a6bd8
quveryFish <yaromdem@gmail.com>
2025-08-26 18:08:35 +0300

    Revert "Merge branch 'main' of https://github.com/quveryFish/Knightmare-Launcher"
    This reverts commit 0a7ce9d2beeaec1dd88e7afe3d7ec12ea72dec08, reversing
    changes made to 8f302355f542a3c6d6547fb477607e8c9713ec63.

--- Assets/ProjFiles/Scripts/player/Anim/PlayerAnimManage.cs
+++ Assets/ProjFiles/Scripts/player/Anim/PlayerAnimManage.cs
@@ -5,8 +5,13 @@ public class PlayerAnimManage : MonoBehaviour
 {
     private const string ANIM_SHOOT = "Shoot";
 
+    private const string ANIM_DASH_RIGHT = "DashRight";
+    private const string ANIM_DASH_LEFT = "DashLeft";
+    private const string ANIM_DASH_FORWARD = "DashForward";
+    private const string ANIM_DASH_BACKWARD = "DashBackward";
 
-    private Animator animator;
+
+private Animator animator;
     private float timerToShoot = 0.5f;
 
     private PlayerMovement playerMovement;
@@ -47,19 +52,19 @@ public class PlayerAnimManage : MonoBehaviour
         {
             if (playerMovement.GetHorizontalInput() > 0)
             {
-                animator.SetTrigger("DashRight");
+                animator.SetTrigger(ANIM_DASH_RIGHT);
             }
             else if (playerMovement.GetHorizontalInput() < 0)
             {
-                animator.SetTrigger("DashLeft");
+                animator.SetTrigger(ANIM_DASH_LEFT);
             }
             else if (playerMovement.GetVerticalInput() > 0)
             {
-                animator.SetTrigger("DashForward");
+                animator.SetTrigger(ANIM_DASH_FORWARD);
             }
             else if (playerMovement.GetVerticalInput() < 0)
             {
-                animator.SetTrigger("DashBackward");
+                animator.SetTrigger(ANIM_DASH_BACKWARD);
             }
             playerDash.isDashing = false;
         }
